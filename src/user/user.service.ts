479a350176306e675c6247519bd6f9c63b232fd3
nehapatlolla <neha.patlolla.ce@omf.com>
2024-09-24 12:36:50 +0530

    changes

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -31,65 +31,9 @@ export class UserService {
     this.producerServiceUrl = process.env.PRODUCER_SERVICE_URL;
   }
 
-  // async createUser(createUserDto: CreateUserDto) {
-  //   const { email, dob } = createUserDto;
-
-  //   // Validate input
-  //   if (!email || !dob) {
-  //     throw new BadRequestException(
-  //       'Email and date of birth must be provided.',
-  //     );
-  //   }
-
-  //   try {
-  //     const existingUserStatus = await this.checkUserStatus({ email, dob });
-  //     console.log('Existing User Status:', existingUserStatus); // Log the status
-  //     if (existingUserStatus === 'Failed to check user status') {
-  //       const id = uuidv4();
-
-  //       const messageBody = {
-  //         operation: 'create',
-  //         user: {
-  //           id,
-  //           firstName: createUserDto.firstName,
-  //           lastName: createUserDto.lastName,
-  //           email: createUserDto.email,
-  //           dob: createUserDto.dob,
-  //           status: 'created',
-  //           createdAt: new Date().toISOString(),
-  //         },
-  //       };
-
-  //       const command = new SendMessageCommand({
-  //         QueueUrl: this.queueUrl,
-  //         MessageBody: JSON.stringify(messageBody),
-  //       });
-  //       // Log the queue URL for debugging
-  //       this.logger.log(`Sending message to SQS Queue: ${this.queueUrl}`);
-  //       // Debugging logs
-  //       console.log('Queue URL:', this.queueUrl); // Log the Queue URL
-  //       console.log('Message Body:', JSON.stringify(messageBody)); // Log the message body
-  //       await this.sqsClient.send(command);
-  //       this.logger.log('User creation request sent to SQS queue');
-
-  //       return { message: 'User creation request sent' };
-  //     } else if (existingUserStatus) {
-  //       this.logger.warn(`User with email ${email} already exists.`);
-  //       return { message: `User with email ${email} already exists.` };
-  //     }
-  //   } catch (error) {
-  //     this.logger.error(
-  //       'Error sending user creation request to SQS queue:',
-  //       error,
-  //     );
-  //     throw new BadRequestException('Failed to send user creation request');
-  //   }
-  // }
-
   async createUser(createUserDto: CreateUserDto) {
     const { email, dob } = createUserDto;
 
-    // Log the input
     this.logger.log(`Creating user with email: ${email} and dob: ${dob}`);
 
     if (!email || !dob) {
@@ -103,7 +47,6 @@ export class UserService {
       this.logger.log(`Existing User Status: ${existingUserStatus}`);
 
       if (existingUserStatus == 'Failed to check user status') {
-        // Ensure this matches your condition
         const id = uuidv4();
 
         const messageBody = {
